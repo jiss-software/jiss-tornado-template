e72db092eae568b4c090ba92421a9af811adb224
Anton Iskov <anton.iskov@seamless.se>
2015-10-24 21:57:51 +0200

    Use options

--- server.py
+++ server.py
@@ -3,12 +3,12 @@ import tornado.web
 import logging
 from handler import HealthCheckHandler
 import motor
+from tornado.options import define, options
 
-PORT = 33001
-MAX_BUFFER_SIZE = 50 * 1024**2
-
-DB_ADDRESS = 'mongodb://localhost:27017'
-DB_NAME = 'test'
+define("port", default=33001, help="Application port")
+define("db_address", default="mongodb://localhost:27017", help="Database address")
+define("db_name", default="test", help="Database name")
+define("max_buffer_size", default=50 * 1024**2, help="")
 
 logging.basicConfig(
     format='%(asctime)s [%(name)s] %(levelname)s: %(message)s',
@@ -27,13 +27,13 @@ app = tornado.web.Application([
     (r"/", HealthCheckHandler, context),
 ], autoreload=True)
 
-app.listen(PORT)
+app.listen(options.port)
 
 if __name__ == "__main__":
     ioLoop = tornado.ioloop.IOLoop.current()
     try:
-        logging.info("Starting HTTP proxy on port %d" % PORT)
+        logging.info("Starting HTTP proxy on port %d" % options.port)
         ioLoop.start()
     except KeyboardInterrupt:
-        logging.info("Shutting down server HTTP proxy on port %d" % PORT)
+        logging.info("Shutting down server HTTP proxy on port %d" % options.port)
         ioLoop.stop()
