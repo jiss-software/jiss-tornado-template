bb7e161b106c69350ddb243388e9f7a57eb7acaf
Anton Iškov <iskov.anton@yandex.com>
2015-10-25 14:23:40 +0100

    Update HealthCheck.py

--- handler/HealthCheck.py
+++ handler/HealthCheck.py
@@ -3,14 +3,29 @@ import json
 
 
 class HealthCheckHandler(tornado.web.RequestHandler):
-    def initialize(self, logger):
+    def initialize(self, logger, mongodb):
         self.logger = logger
+        self.mongodb = mongodb
 
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
     def get(self):
-        self.logger.info('Request to health check: /')
+        self.logger.info('Request to health check')
+
+        # noinspection PyDictCreation
+        components = {
+            'mongodb': None
+        }
+
+        components['mongodb'] = yield self.mongodb.alive()
+
+        for key in components:
+            if components[key] is None:
+                components[key] = False
 
         result = json.dumps({
-            'status': 'OK'
+            'status': False not in components.values(),
+            'components': components
         })
 
         self.logger.info('Response for health check: %s' % result)
