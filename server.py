6664f80eac0b9bc5bdba0b6006cbb31261e3c7a9
Anton Iškov <iskov.anton@yandex.com>
2015-10-25 14:29:28 +0100

    Update server.py

--- server.py
+++ server.py
@@ -4,11 +4,16 @@ import logging
 from handler import HealthCheckHandler
 import motor
 from tornado.options import define, options
+import os
 
 define("port", default=33001, help="Application port")
 define("db_address", default="mongodb://localhost:27017", help="Database address")
 define("db_name", default="test", help="Database name")
 define("max_buffer_size", default=50 * 1024**2, help="")
+define("log_dir", default="log", help="Logger directory")
+
+if not os.path.exists(options.log_dir):
+    os.makedirs(options.log_dir)
 
 logging.basicConfig(
     format='%(asctime)s [%(name)s] %(levelname)s: %(message)s',
@@ -16,11 +21,12 @@ logging.basicConfig(
     level=logging.DEBUG
 )
 
-# mongodb = motor.MotorClient(DB_ADDRESS)
+ioLoop = tornado.ioloop.IOLoop.current()
+mongodb = ioLoop.run_sync(motor.MotorClient(options.db_address).open)
 
 context = dict(
     logger=logging.getLogger('HealthCheck'),
-    # database=mongodb[DB_NAME]
+    mongodb=mongodb
 )
 
 app = tornado.web.Application([
@@ -30,7 +36,6 @@ app = tornado.web.Application([
 app.listen(options.port)
 
 if __name__ == "__main__":
-    ioLoop = tornado.ioloop.IOLoop.current()
     try:
         logging.info("Starting HTTP proxy on port %d" % options.port)
         ioLoop.start()
