27a5ce409700ad10e1d46ece230e563993fe158a
Anton Iškov <iskov.anton@yandex.com>
2015-10-27 15:59:28 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,10 @@
 import tornado.ioloop
 import tornado.web
 import logging
-from handler import HealthCheckHandler
 import motor
-from tornado.options import define, options
+from settings import routing
+from tornado.options import options
 import os
-
-define("port", default=33001, help="Application port")
-define("db_address", default="mongodb://localhost:27017", help="Database address")
-define("db_name", default="test", help="Database name")
-define("max_buffer_size", default=50 * 1024**2, help="")
-define("log_dir", default="log", help="Logger directory")
 
 if not os.path.exists(options.log_dir):
     os.makedirs(options.log_dir)
@@ -23,15 +17,7 @@ logging.basicConfig(
 
 ioLoop = tornado.ioloop.IOLoop.current()
 mongodb = ioLoop.run_sync(motor.MotorClient(options.db_address).open)
-
-context = dict(
-    logger=logging.getLogger('HealthCheck'),
-    mongodb=mongodb
-)
-
-app = tornado.web.Application([
-    (r"/", HealthCheckHandler, context),
-], autoreload=True)
+app = tornado.web.Application(routing, db=mongodb, autoreload=True)
 
 app.listen(options.port)
 
