761098e2e8749e8f2404d6c39542b8913b52fa9c
Anton Iškov <iskov.anton@yandex.com>
2015-10-27 15:57:39 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,12 @@
+from tornado.options import define
+from handler import HealthCheckHandler
+import logging
+
+define("port", default=33001, help="Application port")
+define("db_address", default="mongodb://localhost:27017", help="Database address")
+define("db_name", default="test", help="Database name")
+define("max_buffer_size", default=50 * 1024**2, help="")
+
+routing = [
+    (r"/", HealthCheckHandler, {'logger': logging.getLogger('HealthCheck')}),
+]
