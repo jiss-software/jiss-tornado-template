2e139a33e5ecf69d450c8e33c2b2071e4de9cc4c
Anton Iškov <iskov.anton@yandex.com>
2015-10-29 15:14:12 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@ define("port", default=33001, help="Application port")
 define("db_address", default="mongodb://localhost:27017", help="Database address")
 define("db_name", default="test", help="Database name")
 define("max_buffer_size", default=50 * 1024**2, help="")
+define("autoreload", default=False, help="Autoreload server on change")
 
 routing = [
     (r"/", HealthCheckHandler),
