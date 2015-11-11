215bb6c3b7427540df429e0e5661de49498145e3
Антон Ишков <hasado@yandex.ru>
2015-11-11 15:02:43 +0100

    Parse command line args

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from settings import routing
 from tornado.options import options
 import os
 
+tornado.options.parse_command_line()
+
 if not os.path.exists(options.log_dir):
     os.makedirs(options.log_dir)
 
@@ -16,6 +18,8 @@ logging.basicConfig(
 )
 
 ioLoop = tornado.ioloop.IOLoop.current()
+
+logging.getLogger('INIT').info('Connecting to mongodb at: %s' % options.db_address)
 mongodb = ioLoop.run_sync(motor.MotorClient(options.db_address).open)
 app = tornado.web.Application(routing, db=mongodb, autoreload=options.autoreload)
 
